0bacae80adbb200e52e2dcf704adc6997279a585
Dayo Akinyemi <darhyor2050@gmail.com>
2025-02-08 15:19:07 +0100

    fixed for all browsers

--- frontendDashboard/vite.config.js
+++ frontendDashboard/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    target: 'esnext', // Change this to 'es2015' for wider browser support
+    target: 'es2015', // Change this to 'es2015' for wider browser support
   },
   server: {
     host: true, // Ensures it runs on all network interfaces
